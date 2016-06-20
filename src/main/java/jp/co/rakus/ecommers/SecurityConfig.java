f776ffa56a55d4cca2226ef20d6c5c7116edf246
kohei.sakata <kohei.sakata@rakus.co.jp>
2016-06-20 12:28:29 +0900

    ログイン画面作成/ログイン動作確認済

--- src/main/java/jp/co/rakus/ecommers/SecurityConfig.java
+++ src/main/java/jp/co/rakus/ecommers/SecurityConfig.java
@@ -1,8 +1,6 @@
 package jp.co.rakus.ecommers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -11,56 +9,46 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import jp.co.rakus.ecommers.service.LoginAdminUserService;
 
 @Configuration
 @EnableWebMvcSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
-	@Bean
-	PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
-    @Autowired
-    private static PasswordEncoder passwordEncoder;
-    
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-    	web.ignoring().antMatchers("/css/**", "/img/**");
-    }
+public class SecurityConfig {
 
     @Configuration
     @Order(1)
     public static class AdminUserSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         @Autowired
-        @Qualifier("admin")
         private LoginAdminUserService loginAdminUserService;
+
+        @Override
+        public void configure(WebSecurity web) throws Exception {
+        	web.ignoring().antMatchers("/css/**", "/img/**");
+        }
 
         @Override
         public void configure(AuthenticationManagerBuilder auth) throws Exception {
             auth
                 .userDetailsService(loginAdminUserService)
-                    .passwordEncoder(passwordEncoder);
+                    .passwordEncoder(new BCryptPasswordEncoder());
         }
 
         @Override
         protected void configure(HttpSecurity http) throws Exception {
             http
-                .antMatcher("/admin/**")
+            	.antMatcher("/admin/**")
                 .authorizeRequests()
-                    .anyRequest()
-                    .hasRole("ADMIN")
+                	.antMatchers("/admin/loginForm").permitAll()
+                    .anyRequest().hasRole("ADMIN")
                 .and()
                 .formLogin()
                 	.loginProcessingUrl("/admin/login")
                 	.loginPage("/admin/loginForm")
                 	.failureUrl("/admin/loginForm?error")
-                	.defaultSuccessUrl("/admin/menu")
+                	.defaultSuccessUrl("/admin/menu",true)
                 	.usernameParameter("email")
                 	.passwordParameter("password")
                 .and()
---
+++ src/main/webapp/WEB-INF/views/adminCommon.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+    pageEncoding="utf-8"%>
+    <%@ taglib uri="http://java.sun.com/jstl/fmt" prefix="fmt" %>
+        <%@ taglib uri="http://java.sun.com/jstl/core" prefix="c" %>
+    <%@ taglib uri="http://www.springframework.org/tags/form" prefix="form" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Insert title here</title>
+</head>
