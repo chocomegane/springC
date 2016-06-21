3490cc958d64b3311defa4f82a42b0f533497ba0
yusuke.nakano <yusuke.nakano@rakus.co.jp>
2016-06-21 10:32:35 +0900

    商品の編集機能追加.

---
+++ src/main/java/jp/co/rakus/ecommers/web/UpdateCinemaController.java
@@ -0,0 +1,61 @@
+package jp.co.rakus.ecommers.web;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.rakus.ecommers.domain.Cinema;
+import jp.co.rakus.ecommers.service.OrderListService;
+
+/**
+ * 映画を編集するためのクラス.
+ * @author yusuke.nakano
+ *
+ */
+@Controller
+@Transactional
+@RequestMapping(value = "/update")
+public class UpdateCinemaController {
+	
+	/** ListViewServiceを利用するためのDI */
+	@Autowired
+	private OrderListService service;
+		
+	@ModelAttribute
+	public CinemaForm setUpForm() {
+		return new CinemaForm();
+	}
+	
+	/**
+	 * 初期ページを表示するメソッド.
+	 * @param model requestスコープを扱うための変数
+	 * @return updateCinema.jspへフォワード
+	 */
+	@RequestMapping
+	public String index(Model model) {
+		Cinema cinema = service.findOne(7);	// 試験的に使います
+		model.addAttribute("cinema", cinema);
+		return "updateCinema";
+	}
+	
+	/**
+	 * 映画の商品を編集する処理を行うメソッド.
+	 * @param form CinemaForm型の変数
+	 * @param result BindingResult型の変数
+	 * @param model requestスコープを扱うための変数
+	 * @return updateCinema.jspへフォワード
+	 */
+	@RequestMapping(value = "/execute")
+	public String output(CinemaForm form, BindingResult result, Model model) {
+		Cinema cinema = new Cinema();
+		BeanUtils.copyProperties(form, cinema);
+		service.save(cinema);
+	    model.addAttribute("message", "正常に登録が完了しました");
+		return "updateCinema";
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/updateCinema.jsp
@@ -0,0 +1,29 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ include file="adminCommon.jsp" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>商品登録画面</title>
+</head>
+<body>
+<h2>商品登録画面</h2>
+<form:form modelAttribute="cinemaForm" action="/update/execute">
+	<form:hidden path="id" value="${cinema.id}"/>
+	タイトル：<form:input path="title" value="${cinema.title}"/><br>
+	価格：<form:input path="price" value="${cinema.price}"/><br>
+	ジャンル：<form:input path="genre" value="${cinema.genre}"/><br>
+	上映時間：<form:input path="time" value="${cinema.time}"/><br>
+	公開日：<form:input path="releaseDate" value="${cinema.releaseDate}"/><br>
+	メディアタイプ：<form:input path="mediaType" value="${cinema.mediaType}"/><br>
+	制作会社：<form:input path="company" value="${cinema.company}"/><br>
+	監督：<form:input path="directedBy" value="${cinema.directedBy}"/><br>
+	レーディング：<form:input path="rating" value="${cinema.rating}"/><br>	
+	概要（ストーリー）：<br><textarea name="description" rows="5" cols="30"><c:out value="${cinema.description}"/></textarea><br>
+	イメージ画像：<form:input path="imagePath" value="${cinema.imagePath}"/><br>
+	<input type="submit" value="更新">
+</form:form>
+<c:out value="${message}"/>
+</body>
+</html>
