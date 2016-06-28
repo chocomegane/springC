584be8f3a965f4b52b1eaa7d64362bfcdb7fc186
kohei.sakata <kohei.sakata@rakus.co.jp>
2016-06-28 17:20:55 +0900

    エラーハンドラをまた少し変更

--- src/main/java/jp/co/rakus/ecommers/web/EcControllerAdvice.java
+++ src/main/java/jp/co/rakus/ecommers/web/EcControllerAdvice.java
@@ -8,24 +8,35 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * @author kohei.sakata
+ *
+ */
 @ControllerAdvice
 @Component
 @Order(0)
 public class EcControllerAdvice {
 
+    /**
+     * @param ex
+     * @param request
+     * @return
+     */
     @ExceptionHandler(Exception.class)
     public ModelAndView handleException(Exception ex,
             HttpServletRequest request) {
         String referer = request.getServletPath();
         System.out.println(referer);
-        // 画面Aからきたかを判定する何らかの方法
+        // どのURLで例外が発生したかを判別してViewを振り分け
         if (referer.contains("admin")) {
             ModelAndView mav = new ModelAndView("adminError");
             mav.addObject("message", ex.getMessage());
+            ex.printStackTrace();
             return mav;
         } else {
             ModelAndView mav = new ModelAndView("userError");
             mav.addObject("message", ex.getMessage());
+            ex.printStackTrace();
             return mav;
         }
     }
