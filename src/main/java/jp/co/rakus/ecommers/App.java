c4b6dd39dc662d9dc4cb10e0b4ef224b627952a4
sawamura yuki <yuki.sawamura@rakus.co.jp>
2016-06-28 11:39:26 +0900

    warfail作成できるようにしました

--- src/main/java/jp/co/rakus/ecommers/App.java
+++ src/main/java/jp/co/rakus/ecommers/App.java
@@ -4,15 +4,26 @@ import javax.servlet.MultipartConfigElement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.embedded.MultipartConfigFactory;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer {
+	
+	//warファイルをつくるためのメソッド
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(App.class);
+    }
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
@@ -24,4 +35,7 @@ public class App {
 		factory.setMaxRequestSize(51200 * 1024);	// テキスト込みのファイルの許容容量 (50MB)
 		return factory.createMultipartConfig();
 	}
+	
+	
+	
 }
