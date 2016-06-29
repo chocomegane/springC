15246b557f87201688adf8711b16330f3c1f7527
takeshi.fujimoto <takeshi.fujimoto@rakus.co.jp>
2016-06-29 11:05:40 +0900

    とりあえずコミット

--- src/main/java/jp/co/rakus/ecommers/web/CartForm.java
+++ src/main/java/jp/co/rakus/ecommers/web/CartForm.java
@@ -9,8 +9,10 @@ import lombok.Data;
 @Data
 public class CartForm {
 
+	/** 映画ID */
 	private long cinemaId;
 
+	/** 個数 */
 	@NotNull(message = "注文個数を入力してください")
 	@Range(min = 1, max = 99, message = "1～99の数値を入力してください")
 	private Integer quantity;
